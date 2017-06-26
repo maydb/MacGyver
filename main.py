b6b15200b7a1efb15129c11b2776b45ab91a84d3
maydb <maydb@protonmail.com>
2017-06-26 21:14:29 +0200

    Passing definitions into class methods of Mac_Gyver.

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-from definitions import *
+from game_class import *
 
 if __name__ == '__main__':
 
-
-    pos_hero = [1, 1]
-    size_sprite = 43
-    window = Tk()
-    window.title("Mac_Gyver")
-    maze = maze_load("level_1")
-    (canvas, sprite_hero, photos) = maze_view(maze, window, size_sprite, pos_hero)
-    init_touches(window, canvas, maze, pos_hero, sprite_hero,)
-    window.mainloop()
+    game = Mac_Gyver("level_1")
