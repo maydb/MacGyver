e3af0de5d9077235dfe685bcda8e68db7c92249b
maydb <maydb@protonmail.com>
2017-06-30 19:00:02 +0200

    add labels and treasurs gestion in class an an assault function

--- game_class.py
+++ game_class.py
@@ -3,10 +3,8 @@ import random
 from tkinter import *
 
 
-
 class Mac_Gyver:
-
-    def __init__(self,level_file):
+    def __init__(self, level_file):
         """Loads the maze from the file name.txt
                Name: name of the file containing the maze (without the extension .txt)
                Return data: a list with the maze data"""
@@ -22,14 +20,25 @@ class Mac_Gyver:
 
         self.window = Tk()
         self.window.title("Mac_Gyver")
+        self.treasurs = [0, 0, 0]  # List of treasures harvested !
         self.pos_hero = [1, 1]
+        # Calculation of the size of the maze.
+
+
         self.size_sprite = 43
-        (self.canvas, self.sprite_hero, self.photos) = self.maze_view(self.data, self.window, self.size_sprite, self.pos_hero)
-        self.init_touches(self.window, self.canvas, self.data, self.pos_hero, self.sprite_hero,)
+        (self.canvas, self.sprite_hero, self.photos) = self.maze_view(self.data, self.window, self.size_sprite,
+                                                                      self.pos_hero)
+        self.init_touches(self.window, self.canvas, self.data, self.pos_hero, self.sprite_hero, )
+        self.inventory = "This is your inventory, you need to craft a seringue in order to pass the guardian!"
+
+        self.infos = StringVar()
+        self.label = Label(self.window, textvariable=self.infos, bg="yellow").pack()
+        self.infos.set("Start the game with Up, Down, Left and Right key !")
+        self.label2 = Label(self.window, text=self.inventory, bg="red")
+        self.label2.pack()
         self.window.mainloop()
 
-
-    def maze_view(self,maze, window, size_sprite, pos_hero):
+    def maze_view(self, maze, window, size_sprite, pos_hero):
         """
         Display of a maze.
         maze: Variable containing the maze
@@ -53,46 +62,52 @@ class Mac_Gyver:
             for car in line:  # For each column in the actual line.
                 # Walls
                 if car == "+" or car == "-" or car == "|":
-                    self.can.create_image(n_col + n_col * self.size_sprite, n_line + n_line * self.size_sprite, anchor=NW,
-                                     image=self.photo_wall)
+                    self.can.create_image(n_col + n_col * self.size_sprite, n_line + n_line * self.size_sprite,
+                                          anchor=NW,
+                                          image=self.photo_wall)
                 # Treasurs (we declare it has a global for delete it when macgyver found him.)
-                if car =="1":
-                    global treasurs1
-                    self.treasurs1 = self.can.create_image(n_col + n_col * self.size_sprite, n_line + n_line * self.size_sprite, anchor=NW,
-                                     image=self.photo_treasure)
+                if car == "1":
+                    global treasure1
+                    self.treasure1 = self.can.create_image(n_col + n_col * self.size_sprite,
+                                                           n_line + n_line * self.size_sprite, anchor=NW,
+                                                           image=self.photo_treasure)
                 if car == "2":
-                    global treasurs2
-                    self.treasurs2 = self.can.create_image(n_col + n_col * self.size_sprite, n_line + n_line * self.size_sprite, anchor=NW,
-                                                     image=self.photo_treasure)
+                    global treasure2
+                    self.treasure2 = self.can.create_image(n_col + n_col * self.size_sprite,
+                                                           n_line + n_line * self.size_sprite, anchor=NW,
+                                                           image=self.photo_treasure)
                 if car == "3":
-                    global treasurs3
-                    self.treasurs3 = self.can.create_image(n_col + n_col * self.size_sprite, n_line + n_line * self.size_sprite, anchor=NW,
-                                                     image=self.photo_treasure)
+                    global treasure3
+                    self.treasure3 = self.can.create_image(n_col + n_col * self.size_sprite,
+                                                           n_line + n_line * self.size_sprite, anchor=NW,
+                                                           image=self.photo_treasure)
 
 
                 # Ennemy
                 elif car == "$":
-                    self.can.create_image(n_col + n_col * self.size_sprite, n_line + n_line * self.size_sprite, anchor=NW,
-                                     image=self.photo_ennemy)
+                    self.can.create_image(n_col + n_col * self.size_sprite, n_line + n_line * self.size_sprite,
+                                          anchor=NW,
+                                          image=self.photo_ennemy)
                 # Exit
                 elif car == "O":
                     self.can.create_image(n_col + n_col * size_sprite, n_line + n_line * size_sprite, anchor=NW,
-                                     image=self.photo_exit)
+                                          image=self.photo_exit)
                 n_col += 1
             n_line += 1
 
         # Hero display :
-        self.sprite_hero = self.can.create_image(pos_hero[0] + pos_hero[0] * self.size_sprite, pos_hero[1] + pos_hero[1] * self.size_sprite,
-                                       anchor=NW, image=self.photo_hero)
+        self.sprite_hero = self.can.create_image(pos_hero[0] + pos_hero[0] * self.size_sprite,
+                                                 pos_hero[1] + pos_hero[1] * self.size_sprite,
+                                                 anchor=NW, image=self.photo_hero)
 
         self.can.pack()
 
         return (self.can, self.sprite_hero,
-                {"hero": self.photo_hero, "wall": self.photo_wall, "treasure1": self.photo_treasure, "ennemy": self.photo_ennemy,
+                {"hero": self.photo_hero, "wall": self.photo_wall, "treasure1": self.photo_treasure,
+                 "ennemy": self.photo_ennemy,
                  "exit": self.photo_exit})
 
-
-    def movement(self,event, can, mov, maze, pos_hero, hero):
+    def movement(self, event, can, mov, maze, pos_hero, hero):
         """
         Moving the hero !
         event:
@@ -104,55 +119,114 @@ class Mac_Gyver:
         hero : Sprite representing the character.
         No return value
         """
-        # Calculation of the size of the maze.
         n_cols = len(maze[0])
         n_lignes = len(maze)
-        pos_col, pos_ligne = [pos_hero[0], pos_hero[1]]
+        self.pos_col, self.pos_ligne = [self.pos_hero[0], self.pos_hero[1]]
         # Moving to the right :
-        if mov == "right" :
-            pos_col += 1
+        if mov == "right":
+            self.pos_col += 1
         # Moving to the left :
         elif mov == "left":
-            pos_col -=1
+            self.pos_col -= 1
         # Moving to the top  :
         elif mov == "up":
-            pos_ligne -=1
+            self.pos_ligne -= 1
         # Moving to the bottom :
         elif mov == "down":
-            pos_ligne +=1
+            self.pos_ligne += 1
 
         # Test if the move leads the hero outside the playing area
-        if pos_ligne < 0 or pos_col < 0 or pos_ligne > (n_lignes - 1) or pos_col > (n_cols - 1):
+        if self.pos_ligne < 0 or self.pos_col < 0 or self.pos_ligne > (n_lignes - 1) or self.pos_col > (n_cols - 1):
             return None
 
-        #Tests if a treasure is present on the next move. If so, remove the treasure and complete the hero's inventory.
-        elif maze[pos_ligne][pos_col] == "1" or maze[pos_ligne][pos_col] == "2" or maze[pos_ligne][pos_col] == "3":
-            if maze[pos_ligne][pos_col] == "1" :
-                can.delete(self.treasurs1)
-            elif maze[pos_ligne][pos_col] == "2" :
-                can.delete(self.treasurs2)
-            elif maze[pos_ligne][pos_col] == "3" :
-                can.delete(self.treasurs3)
+        # Tests if a treasure is present on the next move. If so, remove the treasure and complete the hero's inventory.
+        elif maze[self.pos_ligne][self.pos_col] == "1" or maze[self.pos_ligne][self.pos_col] == "2" or \
+                        maze[self.pos_ligne][self.pos_col] == "3":
+            if maze[self.pos_ligne][self.pos_col] == "1":
+                self.find_treasure(self.treasure1)
+                can.delete(self.treasure1)
+            elif maze[self.pos_ligne][self.pos_col] == "2":
+                self.find_treasure(self.treasure2)
+                can.delete(self.treasure2)
+            elif maze[self.pos_ligne][self.pos_col] == "3":
+                self.find_treasure(self.treasure3)
+                can.delete(self.treasure3)
 
-            maze[pos_ligne] = maze[pos_ligne][:pos_col] +" "+maze [pos_ligne][pos_col + 1:]
+            maze[self.pos_ligne] = maze[self.pos_ligne][:self.pos_col] + " " + maze[self.pos_ligne][self.pos_col + 1:]
 
-            can.coords(hero, pos_col + pos_col * 43, pos_ligne + pos_ligne * 43)
+            can.coords(hero, self.pos_col + self.pos_col * 43, self.pos_ligne + self.pos_ligne * 43)
             del pos_hero[0]
             del pos_hero[0]
-            pos_hero.append(pos_col)
-            pos_hero.append(pos_ligne)
+            pos_hero.append(self.pos_col)
+            pos_hero.append(self.pos_ligne)
 
-            return [pos_col, pos_ligne]
+            return [self.pos_col, self.pos_ligne]
 
         # Otherwise if moving is possible on an empty square
-        elif maze[pos_ligne][pos_col] == " ":
-            can.coords(hero, pos_col + pos_col * 43, pos_ligne + pos_ligne * 43)
-            del pos_hero[0]
-            del pos_hero[0]
-            pos_hero.append(pos_col)
-            pos_hero.append(pos_ligne)
+        elif maze[self.pos_ligne][self.pos_col] == " ":
+            can.coords(hero, self.pos_col + self.pos_col * 43, self.pos_ligne + self.pos_ligne * 43)
+            del self.pos_hero[0]
+            del self.pos_hero[0]
+            self.pos_hero.append(self.pos_col)
+            self.pos_hero.append(self.pos_ligne)
 
-    def destroy(self,event, window ) :
+        elif maze[self.pos_ligne][self.pos_col] == "$":
+            self.assault()
+
+    def find_treasure(self, treasure_id):
+        """
+        The function
+
+        :param treasure_id:
+        :return:
+        """
+        if treasure_id == self.treasure1:
+            self.treasurs[0] = 1
+            self.what_treasurs(treasure_id)
+            self.how_many_treasurs()
+
+        elif treasure_id == self.treasure2:
+            self.treasurs[1] = 2
+            self.what_treasurs(treasure_id)
+            self.how_many_treasurs()
+
+        elif treasure_id == self.treasure3:
+            self.treasurs[2] = 3
+            self.what_treasurs(treasure_id)
+            self.how_many_treasurs()
+
+    def what_treasurs(self, treasure_id):
+        if treasure_id == self.treasure1:
+            self.infos.set("Congratulations ! You found a needle !")
+        elif treasure_id == self.treasure2:
+            self.infos = "Congratulations ! You found a small plastic tube !"
+        elif treasure_id == self.treasure3:
+            self.infos = "Congratulations ! You found ether !"
+
+    def how_many_treasurs(self):
+
+        treasure1_sent = "You still need a needle !"
+        treasure2_sent = "You still need a small plastic tube !"
+        treasure3_sent = "You still need ether !"
+        all_treasurs_sent = "You craft a syringe ! You can put the guard to sleep !"
+
+        # self.label.destroy()
+
+
+        if self.treasurs[0] == 0:
+            self.label2 = Label(self.window, text=treasure1_sent, bg="red")
+            self.label2.pack()
+        if self.treasurs[1] == 0:
+            self.label2 = Label(self.window, text=treasure2_sent, bg="red")
+            self.label2.pack()
+        if self.treasurs[2] == 0:
+            self.label2 = Label(self.window, text=treasure3_sent, bg="red")
+            self.label2.pack()
+        elif self.treasurs[0] == 1 and self.treasurs[1] == 2 and self.treasurs[2] == 3:
+            self.label2 = Label(self.window, text=all_treasurs_sent, bg="red")
+            self.label2.pack()
+
+    def destroy(self, event, window):
         """
         Closing the graphic window
         event: Object describing the event that triggered the call to this
@@ -160,7 +234,7 @@ class Mac_Gyver:
         No return value"""
         window.destroy()
 
-    def init_touches(self,window, canvas, maze, pos_hero, hero) :
+    def init_touches(self, window, canvas, maze, pos_hero, hero):
         """
         Initializing keyboard behavior
         canvas : Canvas where to display sprites
@@ -169,8 +243,24 @@ class Mac_Gyver:
         Hero : Sprite representing the hero
         No return value
         """
-        self.window.bind("<Right>", lambda event, can=canvas, l=maze, pos = pos_hero, p = hero: self.movement(event, can, "right", l, pos, p))
-        self.window.bind("<Left>", lambda event, can=canvas, l= maze, pos=pos_hero, p=hero: self.movement(event, can, "left", l, pos, p))
-        self.window.bind("<Up>", lambda event, can=canvas, l= maze, pos=pos_hero, p=hero: self.movement(event, can, "up", l, pos, p))
-        self.window.bind("<Down>", lambda event, can=canvas, l= maze, pos=pos_hero, p=hero: self.movement(event, can, "down", l, pos, p))
+        self.window.bind("<Right>",
+                         lambda event, can=canvas, l=maze, pos=pos_hero, p=hero: self.movement(event, can, "right", l,
+                                                                                               pos, p))
+        self.window.bind("<Left>",
+                         lambda event, can=canvas, l=maze, pos=pos_hero, p=hero: self.movement(event, can, "left", l,
+                                                                                               pos, p))
+        self.window.bind("<Up>",
+                         lambda event, can=canvas, l=maze, pos=pos_hero, p=hero: self.movement(event, can, "up", l, pos,
+                                                                                               p))
+        self.window.bind("<Down>",
+                         lambda event, can=canvas, l=maze, pos=pos_hero, p=hero: self.movement(event, can, "down", l,
+                                                                                               pos, p))
         self.window.bind("<Escape>", lambda event, fen=window: self.destroy(event, fen))
+
+    def assault(self):
+        if self.treasurs[0] == 1 and self.treasurs[1] == 2 and self.treasurs[2] == 3:
+            self.can.coords(self.sprite_hero, self.pos_col + self.pos_col * 43, self.pos_ligne + self.pos_ligne * 43)
+            del self.pos_hero[0]
+            del self.pos_hero[0]
+            self.pos_hero.append(self.pos_col)
+            self.pos_hero.append(self.pos_ligne)
