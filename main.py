6d0d59a3192211384edb0451e6232fbc385da439
maydb <maydb@protonmail.com>
2017-07-02 11:34:05 +0200

    Add a askquestion box in assault and a while in the main.py.

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ from game_class import *
 
 if __name__ == '__main__':
 
-    game = Mac_Gyver("level_1")
+    while 1:
+        game = Mac_Gyver("level_1")
+        if game.game_pursuit == False:
+            break
